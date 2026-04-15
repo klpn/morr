1112a56ff701595b16dc61926f0e6f989bdd90d3
Karl Pettersson <karl.pettersson@klpn.se>
2026-04-15 21:26:26 +0200

    Support multiple age groups for thrctries.

--- R/propyrs.R
+++ R/propyrs.R
@@ -215,22 +215,25 @@ ce.expand <- function(ce, li) {
 }
 
 #' Returns a data frame of countries with minimum and maximum years with a certain cause ratio over a threshold.
-#' @param caf Frame returned byt ctry_caf.
+#' @param caf Frame returned by ctry_caf.
 #' @param thr Include countries with ca1/ca2 ≥ thr.
 #' @param nthr Include countres with ca2 ≥ nthr (to avoid small populations which can give very noisy results).
 #' @param tsex Sex to include.
-#' @param tage Age group to include.
+#' @param tage Age groups to include.
 #' @examples
 #' circall <- ctry_caf("all", "circ", "all")
 #' circall |> thrctries(0.5, 200, 2, 1)
 #' @export
 thrctries <- function(caf, thr, nthr, tsex, tage) {
-    cafi <- caf |> filter(sex==tsex & age==tage & ca2 >= nthr)
+    cafi <- caf |> filter(sex==tsex & age %in% tage & ca2 >= nthr)
     ctrn <- cafi |> group_by(ctry) |>
-        summarise(count_all = n(), yr_min_all = min(yr), yr_max_all = max(yr))
+        summarise(count_all = n(), yr_count_all = n_distinct(yr),
+                  yr_min_all = min(yr), yr_max_all = max(yr))
     ctrt <- cafi |> filter(ca1/ca2 >= thr) |> group_by(ctry) |>
-        summarise(count = n(), yr_min = min(yr), yr_max = max(yr))
+        summarise(count = n(), yr_count = n_distinct(yr),
+                  yr_min = min(yr), yr_max = max(yr))
     ctrnt <- inner_join(ctrn, ctrt, by = "ctry")
+    if (length(tage)==1) ctrnt <- ctrnt |> mutate(yr_count_all = NULL, yr_count = NULL)
     ctrnm <- data.frame(ctry = ctrnt$ctry,
                        name = map_chr(ctrnt$ctry, function(x) ctries[[sprintf("%s", x)]][["name"]]))
     inner_join(ctrnm, ctrnt, by = "ctry")
